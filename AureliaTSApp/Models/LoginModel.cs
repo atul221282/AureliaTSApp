79fb8db5dbdd0eb5a8eddaa2331523945f720087
atul <atul221282@gmail.com>
2016-07-18 00:18:25 +0930

    added mvc partial view rendering

--- AureliaTSApp/Models/LoginModel.cs
+++ AureliaTSApp/Models/LoginModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,9 @@ namespace AureliaTSApp.Models
 {
     public class LoginModel
     {
+        [Required, DataType(DataType.Text)]
+        public string UserName { get; set; }
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
     }
 }
--- AureliaTSApp/src/getmain.ts
+++ AureliaTSApp/src/getmain.ts
@@ -1,11 +1,23 @@
-﻿export class GetMain {
+﻿import {autoinject} from 'aurelia-framework';
+import {InlineViewStrategy} from 'aurelia-framework';
+import {HttpClient, HttpClientConfiguration} from 'aurelia-fetch-client';
+
+@(autoinject)
+export class GetMain {
     userName: string;
-    constructor() {
-        
+    inlineViewStartegy: InlineViewStrategy;
+    httpClient: HttpClient;
+    constructor(private viewStrategy: InlineViewStrategy, private httpCl: HttpClient) {
+        this.inlineViewStartegy = viewStrategy;
+        this.httpClient = httpCl;
     }
 
     activate(bindingContext: any) {
         this.userName = bindingContext;
     }
+
+    //getViewStrategy() {
+    //    return new InlineViewStrategy('<template>Hello</template>');
+    //}
 
 }
