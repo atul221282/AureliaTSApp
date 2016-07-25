e2fb243596db3727cab4d8b67bf8c299938a3763
atul <atul221282@gmail.com>
2016-07-25 19:33:38 +0930

    some stuff

--- AureliaTSApp/custom_typings/jqueryui.d.ts
+++ AureliaTSApp/custom_typings/jqueryui.d.ts
@@ -1,10 +1,10 @@
-﻿// Type definitions for jQueryUI 1.9
+﻿/// <reference path="jquery.d.ts" />
+// Type definitions for jQueryUI 1.9
 // Project: http://jqueryui.com/
 // Definitions by: Boris Yankov <https://github.com/borisyankov/>, John Reilly <https://github.com/johnnyreilly>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 
-/// <reference path="../jquery/jquery.d.ts"/>
 
 declare namespace JQueryUI {
     // Accordion //////////////////////////////////////////////////
